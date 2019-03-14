1bc3db054ee832c11fe5475ff72979a89d95676b
Ahmed Shariff <ahmed.shariff@rocketmail.com>
2019-03-14 16:51:40 +0530

    Fixed wrong placement of rm statments in reinstall script

--- reinstall.sh
+++ reinstall.sh
@@ -1,6 +1,6 @@
 # Simple helper script to reinstall and test the package
-python setup.py sdist bdist_wheel
-pip uninstall mlpipeline_torch_utils -y
 rm -rf dist
 rm -rf build
+python setup.py sdist bdist_wheel
+pip uninstall mlpipeline_torch_utils -y
 pip install dist/*
